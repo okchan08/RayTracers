3914a6837e06d814c90d35e7c1e3e495342b87d4
Yoshiki Okesaku <yoshiki.o1994@gmail.com>
2022-02-16 15:54:30 +0900

    add rayon

--- src/object/shape.rs
+++ src/object/shape.rs
@@ -2,7 +2,7 @@ use crate::base::vec::Vec3;
 use crate::object::hit::HitInfo;
 use crate::object::ray::Ray;
 
-pub trait Shape {
+pub trait Shape: Sync {
   fn hit(&self, ray: &Ray, t0: f64, t1: f64) -> Option<HitInfo>;
 
   fn scatter(&self, incoming_ray: &Ray, hit_info: &HitInfo) -> Option<(Ray, Vec3)>;
